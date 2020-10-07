71afaae702d7ab9798ce04715215474dff4c2b27
Frankie Valentine <frankievalentine9@gmail.com>
2020-10-06 22:24:10 -0700

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { Hello } from "../components/Hello";
+// import './index.css'
+
+const App = () => {
+    return (
+        <div>
+            // make sure Typescript is working by using number type
+            <Hello compiler="Parcel" framework="React" />
+        </div>
+    );
+};
+
+render(<App />, document.getElementById("app"));
